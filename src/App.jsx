01a80db1c7e870dfeb343513873a87a8ff80c786
JULIAN930690 <julianjoaquinluque@gmail.com>
2025-08-01 21:51:27 -0300

    Fix ScrollToTop case sensitivity for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ import "react-toastify/dist/ReactToastify.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import AppRoutes from "./routers/routers";
-import ScrollToTop from "./utils/scrolltotop"; // ✅ en minúscula para Linux/Vercel
+import ScrollToTop from "./utils/scrolltotop"; // ⛳ archivo en minúscula
 
 function App() {
   return (
     <BrowserRouter>
-      <ScrollToTop /> {/* Evita que se quede abajo al cambiar de página */}
+      <ScrollToTop /> {/* ⛳ El componente se sigue llamando con mayúscula */}
       <div className="flex flex-col min-h-screen bg-gray-50">
         <Navbar />
         <main className="flex-grow pt-16 px-4">
